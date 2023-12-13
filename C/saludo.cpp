b7761c4dc5cef483b12709cb6f0f1a35c18d5166
Arke.off <santiagoexe75@gmail.com>
2023-12-13 08:02:55 -0300

    first commit

---
+++ C/saludo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int caja;
+	
+	printf("Hola mundo: 1234");
+	
+	
+	scanf("%d",&caja);
+	
+	return 0;
+}
